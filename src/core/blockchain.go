c297fb5c1a761ba8b0b35e54f8286487dc35be9f
wenbo <songwenbo@ixindong.cn>
2021-12-06 14:45:47 +0800

    区块链代码

--- src/core/blockchain.go
+++ src/core/blockchain.go
@@ -1,1 +1,13 @@
 package core
+
+// BlockChain 链接区块
+type BlockChain struct {
+	Blocks []*Block
+}
+
+// AddBlock 添加区块到链上
+func (bc *BlockChain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
