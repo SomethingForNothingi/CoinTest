73f2cd3340a2745f82199070f1c8b801bb25e002
wenbo <songwenbo@ixindong.cn>
2021-12-06 15:24:47 +0800

    runrunrun

--- src/coin/main.go
+++ src/coin/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	core "CoinTest/src/core"
+	"fmt"
+)
+
 func main() {
-	
+	bc := core.NewBlockChain()
+
+	bc.AddBlock("Send 1 BTC to my")
+	bc.AddBlock("Send 2 BTC to my")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev.hash： %x\n", block.PrevBlockHash)
+		fmt.Printf("Data： %s\n", block.Data)
+		fmt.Printf("Hash： %x\n", block.Hash)
+		fmt.Println()
+	}
+
 }
